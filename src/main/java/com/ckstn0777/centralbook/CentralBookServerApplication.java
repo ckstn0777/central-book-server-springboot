6cbb78184e56796232a71cdb19e2b9a816eb296f
ckstn0777 <ckstn0777@naver.com>
2022-07-31 17:01:26 +0900

    프로젝트 세팅

---
+++ src/main/java/com/ckstn0777/centralbook/CentralBookServerApplication.java
@@ -0,0 +1,13 @@
+package com.ckstn0777.centralbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralBookServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralBookServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ckstn0777/centralbook/CentralBookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ckstn0777.centralbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralBookServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
